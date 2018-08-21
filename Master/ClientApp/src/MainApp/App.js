0916d37d82534144ef72b1c7d16b9b43638bf063
Ishola Japheth <japhethish@gmail.com>
2018-08-21 19:03:57 +0100

    Remove version redundant code for react hot reload

--- Master/ClientApp/src/MainApp/App.js
+++ Master/ClientApp/src/MainApp/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+
 class App extends React.Component {
     render () {
         return <h1>Hello From ReactDOM</h1>;
--- Master/ClientApp/src/index.js
+++ Master/ClientApp/src/index.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { AppContainer } from "react-hot-loader";
 import App from "./MainApp/App";
 
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        document.getElementById('app-container')
-    );
-}
-
-render(App);
-
-if (module.hot) {
-    module.hot.accept('./MainApp/App', () => {
-      const NextApp = require('./MainApp/App').default;
-      render(NextApp);
-    });
-  }
+ReactDOM.render(<App />, document.getElementById("app-container"));
